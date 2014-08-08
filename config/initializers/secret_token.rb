1b7320e918ee2aaf78657d585a27ba898781df72
Rishi Dhanaraj <rishid97@gmail.com>
2014-08-08 12:39:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neckbeard::Application.config.secret_key_base = '60568f1dbb02c26990b005667bf4530286a9e32644c4b2391d5d49881ec8a1a6f86a5d330c06b5bde0c05e3339fa7fb0d6485cc5eb6534ff463e66d35f8e3eef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Neckbeard::Application.config.session_store :cookie_store, key: '_Neckbeard_session'
